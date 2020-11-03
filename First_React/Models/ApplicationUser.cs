8f5dfc9d110ab10d84cbb7016b502db7ce408693
Mutebi David <mdavid.trevor@gmail.com>
2020-11-03 17:35:28 +0300

    First Commit

---
+++ First_React/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace First_React.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
